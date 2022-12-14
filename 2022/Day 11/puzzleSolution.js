d63b3056d9f02886a07e84ef9b0df9ac32e9bf91
Andre Dean <andre_dean@me.com>
2022-12-13 18:53:13 -0800

    Day 11 completed solution for part 2. Part 2 completed after watching https://www.youtube.com/watch?v=PDU0qt_Bh4Y

--- 2022/Day 11/puzzleSolution.js
+++ 2022/Day 11/puzzleSolution.js
@@ -293,13 +293,99 @@ const partOne = () => {
 partOne();
 
 
-// CURRENTLY TAKES TOO LONG TO RUN TO GET OUTPUT. NEED TO RE-FACTOR
+/*
+    --- Part Two ---
+    You're worried you might not ever get your items back. So worried, in fact, that your relief that a monkey's inspection didn't damage an item no longer causes your worry level to be divided by three.
+
+    Unfortunately, that relief was all that was keeping your worry levels from reaching ridiculous levels. You'll need to find another way to keep your worry levels manageable.
+
+    At this rate, you might be putting up with these monkeys for a very long time - possibly 10000 rounds!
+
+    With these new rules, you can still figure out the monkey business after 10000 rounds. Using the same example above:
+
+    == After round 1 ==
+    Monkey 0 inspected items 2 times.
+    Monkey 1 inspected items 4 times.
+    Monkey 2 inspected items 3 times.
+    Monkey 3 inspected items 6 times.
+
+    == After round 20 ==
+    Monkey 0 inspected items 99 times.
+    Monkey 1 inspected items 97 times.
+    Monkey 2 inspected items 8 times.
+    Monkey 3 inspected items 103 times.
+
+    == After round 1000 ==
+    Monkey 0 inspected items 5204 times.
+    Monkey 1 inspected items 4792 times.
+    Monkey 2 inspected items 199 times.
+    Monkey 3 inspected items 5192 times.
+
+    == After round 2000 ==
+    Monkey 0 inspected items 10419 times.
+    Monkey 1 inspected items 9577 times.
+    Monkey 2 inspected items 392 times.
+    Monkey 3 inspected items 10391 times.
+
+    == After round 3000 ==
+    Monkey 0 inspected items 15638 times.
+    Monkey 1 inspected items 14358 times.
+    Monkey 2 inspected items 587 times.
+    Monkey 3 inspected items 15593 times.
+
+    == After round 4000 ==
+    Monkey 0 inspected items 20858 times.
+    Monkey 1 inspected items 19138 times.
+    Monkey 2 inspected items 780 times.
+    Monkey 3 inspected items 20797 times.
+
+    == After round 5000 ==
+    Monkey 0 inspected items 26075 times.
+    Monkey 1 inspected items 23921 times.
+    Monkey 2 inspected items 974 times.
+    Monkey 3 inspected items 26000 times.
+
+    == After round 6000 ==
+    Monkey 0 inspected items 31294 times.
+    Monkey 1 inspected items 28702 times.
+    Monkey 2 inspected items 1165 times.
+    Monkey 3 inspected items 31204 times.
+
+    == After round 7000 ==
+    Monkey 0 inspected items 36508 times.
+    Monkey 1 inspected items 33488 times.
+    Monkey 2 inspected items 1360 times.
+    Monkey 3 inspected items 36400 times.
+
+    == After round 8000 ==
+    Monkey 0 inspected items 41728 times.
+    Monkey 1 inspected items 38268 times.
+    Monkey 2 inspected items 1553 times.
+    Monkey 3 inspected items 41606 times.
+
+    == After round 9000 ==
+    Monkey 0 inspected items 46945 times.
+    Monkey 1 inspected items 43051 times.
+    Monkey 2 inspected items 1746 times.
+    Monkey 3 inspected items 46807 times.
+
+    == After round 10000 ==
+    Monkey 0 inspected items 52166 times.
+    Monkey 1 inspected items 47830 times.
+    Monkey 2 inspected items 1938 times.
+    Monkey 3 inspected items 52013 times.
+    After 10000 rounds, the two most active monkeys inspected items 52166 and 52013 times. Multiplying these together, the level of monkey business in this situation is now 2713310158.
+
+    Worry levels are no longer divided by three after each item is inspected; you'll need to find another way to keep your worry levels manageable. Starting again from the initial state in your puzzle input, what is the level of monkey business after 10000 rounds?
+
+    Your puzzle answer was 14081365540.
+*/
 const partTwo = () => {
     const notes = []
     let count = 0;
     let monkey = {};
 
-    fs.readFileSync("puzzleExample.txt", {encoding: "utf-8"})
+    fs.readFileSync("puzzleInput.txt", {encoding: "utf-8"})
         .replace(/\r/g,"")
         .split("\n")
         .map(line => line.trim())
@@ -314,7 +400,7 @@ const partTwo = () => {
                     break;
                 case 1:
                     const items = item.slice(2)
-                    monkey.items = items.map(itm => BigInt(itm))
+                    monkey.items = items.map(itm => Number(itm))
                     count++;
                     break;
                 case 2:
@@ -322,15 +408,15 @@ const partTwo = () => {
                     count++;
                     break;
                 case 3:
-                    monkey.test = BigInt(item[3])
+                    monkey.test = Number(item[3])
                     count++;
                     break;
                 case 4:
-                    monkey.result = {"true": BigInt(item.at(-1))}
+                    monkey.result = {"true": Number(item.at(-1))}
                     count++;
                     break;
                 case 5:
-                    monkey.result["false"] = BigInt(item.at(-1))
+                    monkey.result["false"] = Number(item.at(-1))
                     notes.push(monkey);
                     monkey = {};
                     count = 0;
@@ -340,7 +426,15 @@ const partTwo = () => {
             }            
         });
 
-    // console.log(notes);
+    /***********************************************************************************************************
+        This idea for the "divider" was obtained from https://www.youtube.com/watch?v=PDU0qt_Bh4Y
+
+        Since we are no longer dividing by 3 constant multiplication will make our numbers go out of bounds
+        so to keep worry levels managable, get the product of all our divisibility tests and use that to make
+        our worryLevel = worryLevel % divider.
+    ************************************************************************************************************/
+    
+    const divider = notes.map(m => m.test).reduce((a,b) => a * b, 1);
 
     const operation = {
         "+": (a,b) => a + b,
@@ -349,16 +443,15 @@ const partTwo = () => {
         "/": (a,b) => a / b,
     }
 
-    for(let i = 0; i < 1000; i++){
+    for(let i = 0; i < 10000; i++){
         for(const note of notes){
             for(const item of note.items){
                 const op = note.op[0];
                 const opOn = note.op[1] || item;
-                const worryLevel = operation[op](item, BigInt(opOn))
-                console.log(worryLevel);
-                // const newWorryLevel = Math.floor(worryLevel / 3);
-                const isDivisible = worryLevel % note.test === 0n;
-                notes[note.result[isDivisible]].items.push(worryLevel);
+                let worryLevel = operation[op](item, opOn)
+                worryLevel = worryLevel % divider
+                const destination = worryLevel % note.test == 0;
+                notes[note.result[destination]].items.push(worryLevel);
                 note.itemsInspected++;
             }
             note.items = [];
@@ -368,9 +461,8 @@ const partTwo = () => {
     }
 
     notes.sort((a,b) => a.itemsInspected - b.itemsInspected)
-    // console.log(notes);
 
     console.log(notes.at(-1).itemsInspected * notes.at(-2).itemsInspected);
 }
 
-// partTwo();
+partTwo();
